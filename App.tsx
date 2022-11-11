b27b9aa8a36606cc434b30dc18f05a3760eb7538
Helena Paixao <helena.paixao@corteva.com>
2022-11-11 18:07:47 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,40 @@
+import React, { useEffect, useState, useCallback } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        // handle any exceptions
+      } finally {
+        setSplashReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isSplashReady) {
+      // Hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+  }, [isSplashReady]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container } onLayout={onLayoutRootView}   >
+      <Text>FastFeet</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#545',
     alignItems: 'center',
     justifyContent: 'center',
   },
