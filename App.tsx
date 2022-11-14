eb861b52437d25ace55932aa47f38b2560c358a4
Helena Paixao <helena.paixao@corteva.com>
2022-11-14 10:47:08 -0300

    feat: add fonts in project

--- App.tsx
+++ App.tsx
@@ -2,21 +2,38 @@ import React, { useEffect, useState, useCallback } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen'
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium
+  
+} from '@expo-google-fonts/inter'
+import {
+  Roboto_700Bold, Roboto_500Medium
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Roboto_700Bold,
+    Roboto_500Medium
+  })
+
   const [isSplashReady, setSplashReady] = useState(false);
 
 useEffect(() => {
     async function prepare() {
       try {
         await SplashScreen.preventAutoHideAsync();
+        fontsLoaded;
       } catch (e) {
         // handle any exceptions
       } finally {
         setSplashReady(true);
+
       }
     }
     prepare();
+
   }, []);
 
 
---
+++ src/theme/index.tsx
@@ -0,0 +1,29 @@
+export default {
+    COLORS: {
+        WHITE: '#DCDCDC',
+        PRIMARY: '#4C33CC',
+        SECONDARY: '#FFC042',
+        GRAY: '#4C4766',
+        GRAY_: '#6F6C80',
+        TEXT_COMPLEMENT: '#BEBCCC',
+        TEXT_SUBTITLE: '#D4CCFF',
+        SUCESS: '#00DA6D',
+    },
+    FONT_FAMILY: {
+        REGULAR: 'Roboto_400Regular',
+        MEDIUM: 'Roboto_500Medium',
+        BOLD: 'Roboto_700Bold',
+    },
+    FONT_SIZE: {
+        TITLE: 32,
+        SUBTITLE: 18,
+        BASE: 16,
+        SMALL: 12,
+    },
+    FONT_WEIGHT: {
+        REGULAR: '400',
+        MEDIUM: '500',
+        BOLD: '700',
+    },
+
+}
